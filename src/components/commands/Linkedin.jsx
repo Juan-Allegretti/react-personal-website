2bdb6e5397c034399c6a27d14873fb896d059803
Juan Allegretti <juan.elca@gmail.com>
2021-11-13 19:47:37 -0300

    first commit

--- src/components/commands/Linkedin.jsx
+++ src/components/commands/Linkedin.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Linkedin = () => <div>My Linkedin account is <a href="https://www.linkedin.com/in/juanallegretti">@JuanAllegretti</a></div>;
+const Linkedin = () => <div>My Linkedin account: <a href="https://www.linkedin.com/in/juanallegretti">@JuanAllegretti</a></div>;
 
 export default Linkedin;
